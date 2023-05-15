20bbb32ba53448c0bd2a23c812f2860245867f4b
Johannes Burström <johannes@ljud.org>
2023-05-15 09:24:32 +0200

    Change BNO gData initialization
    Supporting C++11

--- plugins/BNO/BNO.cpp
+++ plugins/BNO/BNO.cpp
@@ -44,11 +44,11 @@ struct BNO : public Unit {
     volatile int threadShouldStop;
 };
 
-bnoState_t gData = {0.1, 0.2, 0.3,
-    0.2, 0.0, 0.4,
-    0.3, 0.0, 0.5,
-    0.41, 0.51, 0.61};
-
+bnoState_t gData = {
+	{0.0}, {0.0}, {0.0},
+	{0.0}, {0.0}, {0.0},
+	{0.0}, {0.0}, {0.0},
+	{0.0}, {0.0}, {0.0} };
 
 enum bnoChannel {
     CH_ACC,
